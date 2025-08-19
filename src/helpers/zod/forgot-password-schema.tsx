12513ef1b75c45162a7b876c0adff4023f20870c
PaulDelamare <paulodela2006@gmail.com>
2025-08-19 21:57:13 +0200

    feat: supprimer les schémas de validation pour la réinitialisation et l'oubli de mot de passe

--- src/helpers/zod/forgot-password-schema.tsx
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const ForgotPasswordSchema = z.object({
-    email: z
-        .string({ error: 'Vous devez entrer une adresse e-mail valide.' })
-        .email({ message: "Vous devez entrer une adresse e-mail valide." })
-});

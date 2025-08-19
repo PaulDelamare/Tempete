8bedd6e93f92de41e875fe1d38938c208fcba37f
Quentin Legiemble <q.legiemble@gmail.com>
2025-08-19 14:43:14 +0200

    map component

---
+++ src/app/(main)/map/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MapPage = () => {
+    return (
+        <div>
+            <h1>Ici la map</h1>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default MapPage

ac3f192f679bb0a41a08124b35e508712195147b
Quentin Legiemble <q.legiemble@gmail.com>
2025-08-19 15:49:59 +0200

    basic map integration

--- src/app/(main)/map/page.tsx
+++ src/app/(main)/map/page.tsx
@@ -1,11 +1,22 @@
+'use client'
+
 import React from 'react'
+import { Map } from '@vis.gl/react-maplibre'
 
 const MapPage = () => {
     return (
-        <div>
-            <h1>Ici la map</h1>
-            <div>
-
+        <div className="w-full h-screen flex flex-col">
+            <h1 className="text-2xl font-bold p-4">Carte Tempête</h1>
+            <div className="flex-1">
+                <Map
+                    initialViewState={{
+                        longitude: 2.3522, // Paris
+                        latitude: 48.8566,
+                        zoom: 10
+                    }}
+                    style={{ width: '100%', height: '100%' }}
+                    mapStyle="https://demotiles.maplibre.org/style.json"
+                />
             </div>
         </div>
     )

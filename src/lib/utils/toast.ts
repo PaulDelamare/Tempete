a373f75ae088bb71952604cbd93f2fc79f50a673
PaulDelamare <paulodela2006@gmail.com>
2025-08-19 21:57:37 +0200

    feat: ajouter des fonctions de toast pour les notifications de succès et d'erreur

---
+++ src/lib/utils/toast.ts
@@ -0,0 +1,19 @@
+import { toast } from "sonner";
+
+export const toastSuccess = (title: string, description: string) => {
+     toast(title, {
+          description,
+          style: { background: "#d1fae5", color: "#065f46" },
+          position: "bottom-center",
+          descriptionClassName: "text-xs font-medium !text-green-800",
+     });
+};
+
+export const toastError = (title: string, description: string) => {
+     toast(title, {
+          description,
+          style: { background: "#fee2e2", color: "#82181a" }, // bg plus clair
+          position: "bottom-center",
+          descriptionClassName: "text-xs font-medium !text-red-800",
+     });
+};

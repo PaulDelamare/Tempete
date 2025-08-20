e81cd8885e83710933164bcad4fa079a2286f3a5
Paul_Delamare <paulodela2006@gmail.com>
2025-08-20 18:47:04 +0200

    style: reformater le schéma de validation du formulaire d'artiste pour une meilleure lisibilité

--- src/app/api/artist/schema/artist-form-schema.ts
+++ src/app/api/artist/schema/artist-form-schema.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const ArtistFormSchema = z.object({
-  name: z.string().min(2, "Le nom est requis"),
-  nickname: z.string().optional(),
-  bio: z.string().optional(),
-  imgurl: z.string().url().optional(),
-  links: z.string().optional(),
+    name: z.string().min(2, "Le nom est requis"),
+    nickname: z.string().optional(),
+    bio: z.string().optional(),
+    imgurl: z.string().url().optional(),
+    links: z.string().optional(),
 });

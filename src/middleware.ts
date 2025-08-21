7ddad966447023d71a2ad14a8b66464ac723a076
Paul_Delamare <paulodela2006@gmail.com>
2025-08-21 15:51:04 +0200

    refactor: améliorer la logique de middleware pour la gestion des sessions

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,47 @@
-import { NextRequest, NextResponse } from "next/server"
-import { auth } from "@/lib/auth"
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/lib/auth";
 import { betterFetch } from "@better-fetch/fetch";
 
 type Session = typeof auth.$Infer.Session;
 
-
 export async function middleware(request: NextRequest) {
-     const pathname = request.nextUrl.pathname
+     const { pathname, origin } = request.nextUrl;
 
-     if (pathname.startsWith("/dashboard")) {
-          const { data: session } = await betterFetch<Session>("/api/auth/get-session", {
-               baseURL: request.nextUrl.origin,
-               headers: {
-                    cookie: request.headers.get("cookie") || "",
-               },
-          });
+     if (pathname === "/api/auth/get-session") {
+          return NextResponse.next();
+     }
 
-          if (!session) {
+     const getProtectedRoutes = ["/api/user", "/dashboard"];
+
+     const needsSession =
+          request.method !== "GET" || getProtectedRoutes.some((route) => pathname.startsWith(route));
+
+     if (!needsSession) {
+          return NextResponse.next();
+     }
+
+     const { data: session } = await betterFetch<Session>("/api/auth/get-session", {
+          baseURL: origin,
+          headers: {
+               cookie: request.headers.get("cookie") || "",
+          },
+     });
+
+     if (!session) {
+          if (request.headers.get("accept")?.includes("text/html")) {
                return NextResponse.redirect(new URL("/sign-in", request.url));
+          } else {
+               return new NextResponse(JSON.stringify({ error: "Unauthorized" }), {
+                    status: 401,
+                    headers: { "Content-Type": "application/json" },
+               });
           }
      }
 
-     return NextResponse.next()
+     return NextResponse.next();
 }
 
 export const config = {
      matcher: ["/api/:path*", "/dashboard/:path*"],
-     runtime: 'nodejs',
-}
+     runtime: "nodejs",
+};

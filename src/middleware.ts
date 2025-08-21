2372d7e1e73cbdb489c2e34ffac6fda059dd4bbc
HKD76 <hugocadet76750@gmail.com>
2025-08-21 15:10:17 +0200

    Event Page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,49 +1,35 @@
-import { NextRequest, NextResponse } from "next/server"
-import { auth } from "@/lib/auth"
-import {betterFetch} from "@better-fetch/fetch";
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/lib/auth";
+import { betterFetch } from "@better-fetch/fetch";
 
 type Session = typeof auth.$Infer.Session;
 
-
 export async function middleware(request: NextRequest) {
-     const pathname = request.nextUrl.pathname
+  const pathname = request.nextUrl.pathname;
 
+  // 🔒 Cas 2 : sécuriser certaines pages avec better-auth
+  if (pathname.startsWith("/dashboard")) {
+    console.log("in dashboard middleware");
+    const { data: session } = await betterFetch<Session>(
+      "/api/auth/get-session",
+      {
+        baseURL: request.nextUrl.origin,
+        headers: {
+          cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
+        },
+      }
+    );
+    console.log("session:", session);
+    if (!session) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+  }
 
-     // 🔒 Cas 1 : sécuriser les routes API avec une clé
-     if (pathname.startsWith("/api") && !pathname.startsWith("/api/auth")) {
-          console.log("in api middleware")
-          const apiKey = request.headers.get("x-api-key")
-
-          if (apiKey !== process.env.API_KEY) {
-               return NextResponse.json(
-                   { error: "Unauthorized: Invalid API Key" },
-                   { status: 401 }
-               )
-          }
-
-          return NextResponse.next()
-     }
-
-     // 🔒 Cas 2 : sécuriser certaines pages avec better-auth
-     if (pathname.startsWith("/dashboard")) {
-          console.log("in dashboard middleware")
-          const { data: session } = await betterFetch<Session>("/api/auth/get-session", {
-               baseURL: request.nextUrl.origin,
-               headers: {
-                    cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
-               },
-          });
-          console.log("session:", session)
-          if (!session) {
-               return NextResponse.redirect(new URL("/sign-in", request.url));
-          }
-     }
-
-     return NextResponse.next()
+  return NextResponse.next();
 }
 
 // Appliquer sur les routes API et dashboard
 export const config = {
-     matcher: ["/api/:path*", "/dashboard/:path*"],
-     runtime: 'nodejs',
-}
+  matcher: ["/api/:path*", "/dashboard/:path*"],
+  runtime: "nodejs",
+};
--- src/navigation/sidebar/sidebar-items.tsx
+++ src/navigation/sidebar/sidebar-items.tsx
@@ -1,171 +1,177 @@
 import {
-    ShoppingBag,
-    Forklift,
-    Mail,
-    MessageSquare,
-    Calendar,
-    Kanban,
-    ReceiptText,
-    Users,
-    Lock,
-    Fingerprint,
-    SquareArrowUpRight,
-    LayoutDashboard,
-    ChartBar,
-    Banknote,
-    Gauge,
-    GraduationCap,
-    type LucideIcon, LandPlot,
+  ShoppingBag,
+  Forklift,
+  Mail,
+  MessageSquare,
+  Calendar,
+  Kanban,
+  ReceiptText,
+  Users,
+  Lock,
+  Fingerprint,
+  SquareArrowUpRight,
+  LayoutDashboard,
+  ChartBar,
+  Banknote,
+  Gauge,
+  GraduationCap,
+  type LucideIcon,
+  LandPlot,
 } from "lucide-react";
 
 export interface NavSubItem {
-    title: string;
-    url: string;
-    icon?: LucideIcon;
-    comingSoon?: boolean;
-    newTab?: boolean;
-    isNew?: boolean;
+  title: string;
+  url: string;
+  icon?: LucideIcon;
+  comingSoon?: boolean;
+  newTab?: boolean;
+  isNew?: boolean;
 }
 
 export interface NavMainItem {
-    title: string;
-    url: string;
-    icon?: LucideIcon;
-    subItems?: NavSubItem[];
-    comingSoon?: boolean;
-    newTab?: boolean;
-    isNew?: boolean;
+  title: string;
+  url: string;
+  icon?: LucideIcon;
+  subItems?: NavSubItem[];
+  comingSoon?: boolean;
+  newTab?: boolean;
+  isNew?: boolean;
 }
 
 export interface NavGroup {
-    id: number;
-    label?: string;
-    items: NavMainItem[];
+  id: number;
+  label?: string;
+  items: NavMainItem[];
 }
 
 export const sidebarItems: NavGroup[] = [
-    {
-        id: 1,
-        label: "Dashboards",
-        items: [
-            {
-                title: "Default",
-                url: "/dashboard/default",
-                icon: LayoutDashboard,
-            },
-            {
-                title: "CRM",
-                url: "/dashboard/crm",
-                icon: ChartBar,
-            },
-            {
-                title: "Zones",
-                url: "/dashboard/area",
-                icon: LandPlot,
-            },
-            {
-                title: "Analytics",
-                url: "/dashboard/analytics",
-                icon: Gauge,
-                comingSoon: true,
-            },
-            {
-                title: "E-commerce",
-                url: "/dashboard/e-commerce",
-                icon: ShoppingBag,
-                comingSoon: true,
-            },
-            {
-                title: "Academy",
-                url: "/dashboard/academy",
-                icon: GraduationCap,
-                comingSoon: true,
-            },
-            {
-                title: "Logistics",
-                url: "/dashboard/logistics",
-                icon: Forklift,
-                comingSoon: true,
-            },
+  {
+    id: 1,
+    label: "Dashboards",
+    items: [
+      {
+        title: "Default",
+        url: "/dashboard/default",
+        icon: LayoutDashboard,
+      },
+      {
+        title: "CRM",
+        url: "/dashboard/crm",
+        icon: ChartBar,
+      },
+      {
+        title: "Zones",
+        url: "/dashboard/area",
+        icon: LandPlot,
+      },
+      {
+        title: "Événements",
+        url: "/event",
+        icon: Calendar,
+      },
+      {
+        title: "Analytics",
+        url: "/dashboard/analytics",
+        icon: Gauge,
+        comingSoon: true,
+      },
+      {
+        title: "E-commerce",
+        url: "/dashboard/e-commerce",
+        icon: ShoppingBag,
+        comingSoon: true,
+      },
+      {
+        title: "Academy",
+        url: "/dashboard/academy",
+        icon: GraduationCap,
+        comingSoon: true,
+      },
+      {
+        title: "Logistics",
+        url: "/dashboard/logistics",
+        icon: Forklift,
+        comingSoon: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    label: "Pages",
+    items: [
+      {
+        title: "Email",
+        url: "/mail",
+        icon: Mail,
+        comingSoon: true,
+      },
+      {
+        title: "Chat",
+        url: "/chat",
+        icon: MessageSquare,
+        comingSoon: true,
+      },
+      {
+        title: "Calendar",
+        url: "/calendar",
+        icon: Calendar,
+        comingSoon: true,
+      },
+      {
+        title: "Kanban",
+        url: "/kanban",
+        icon: Kanban,
+        comingSoon: true,
+      },
+      {
+        title: "Invoice",
+        url: "/invoice",
+        icon: ReceiptText,
+        comingSoon: true,
+      },
+      {
+        title: "Users",
+        url: "/users",
+        icon: Users,
+        comingSoon: true,
+      },
+      {
+        title: "Roles",
+        url: "/roles",
+        icon: Lock,
+        comingSoon: true,
+      },
+      {
+        title: "Authentication",
+        url: "/auth",
+        icon: Fingerprint,
+        subItems: [
+          { title: "Login v1", url: "/auth/v1/login", newTab: true },
+          { title: "Login v2", url: "/auth/v2/login", newTab: true },
+          {
+            title: "Register v1",
+            url: "/auth/v1/register",
+            newTab: true,
+          },
+          {
+            title: "Register v2",
+            url: "/auth/v2/register",
+            newTab: true,
+          },
         ],
-    },
-    {
-        id: 2,
-        label: "Pages",
-        items: [
-            {
-                title: "Email",
-                url: "/mail",
-                icon: Mail,
-                comingSoon: true,
-            },
-            {
-                title: "Chat",
-                url: "/chat",
-                icon: MessageSquare,
-                comingSoon: true,
-            },
-            {
-                title: "Calendar",
-                url: "/calendar",
-                icon: Calendar,
-                comingSoon: true,
-            },
-            {
-                title: "Kanban",
-                url: "/kanban",
-                icon: Kanban,
-                comingSoon: true,
-            },
-            {
-                title: "Invoice",
-                url: "/invoice",
-                icon: ReceiptText,
-                comingSoon: true,
-            },
-            {
-                title: "Users",
-                url: "/users",
-                icon: Users,
-                comingSoon: true,
-            },
-            {
-                title: "Roles",
-                url: "/roles",
-                icon: Lock,
-                comingSoon: true,
-            },
-            {
-                title: "Authentication",
-                url: "/auth",
-                icon: Fingerprint,
-                subItems: [
-                    { title: "Login v1", url: "/auth/v1/login", newTab: true },
-                    { title: "Login v2", url: "/auth/v2/login", newTab: true },
-                    {
-                        title: "Register v1",
-                        url: "/auth/v1/register",
-                        newTab: true,
-                    },
-                    {
-                        title: "Register v2",
-                        url: "/auth/v2/register",
-                        newTab: true,
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        id: 3,
-        label: "Misc",
-        items: [
-            {
-                title: "Others",
-                url: "/others",
-                icon: SquareArrowUpRight,
-                comingSoon: true,
-            },
-        ],
-    },
+      },
+    ],
+  },
+  {
+    id: 3,
+    label: "Misc",
+    items: [
+      {
+        title: "Others",
+        url: "/others",
+        icon: SquareArrowUpRight,
+        comingSoon: true,
+      },
+    ],
+  },
 ];

2b018edfcfe6b5dea1a812c742b25c588ee5081e
Yoann Gimenez <ygimenez10@gmail.com>
2025-08-21 10:52:21 +0200

    backoffice suite wip

--- src/app/(main)/dashboard/_components/table/schema.ts
+++ src/app/(main)/dashboard/_components/table/schema.ts
@@ -3,8 +3,8 @@ import z from "zod";
 export const recentLeadSchema = z.object({
   id: z.string(),
   name: z.string(),
-  company: z.string(),
-  status: z.string(),
-  source: z.string(),
-  lastActivity: z.string(),
+  capacity: z.number(),
+  type: z.string(),
+  created_at: z.string(),
+  modified_at: z.string(),
 });
